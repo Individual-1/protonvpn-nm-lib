51b09a8341d8ee9a8460b937289471da0682a2d2
Indy <1389922+Individual-1@users.noreply.github.com>
2023-05-17 13:25:45 -0700

    Simplifying pmp addition and adding log message to verify result

--- protonvpn_nm_lib/core/connection_backend/nm_client/openvpn/configure_openvpn_connection.py
+++ protonvpn_nm_lib/core/connection_backend/nm_client/openvpn/configure_openvpn_connection.py
@@ -89,12 +89,7 @@ class ConfigureOpenVPNConnection:
             self.username = self.username + "+nr"
         
         # append NAT-PMP suffix
-        if (
-            #self.__env.api_session.clientconfig.features.nat_pmp
-            #and self.__env.settings.nat_pmp == UserSettingStatusEnum.ENABLED
-            True
-        ):
-            self.username = self.username + f"+{ClientSuffixEnum.NATPMP.value}"
+        self.username = self.username + "+pmp"
 
         # append non standard ports (aka safe mode) suffix
         if self.__env.api_session.clientconfig.features.safe_mode:
@@ -102,6 +97,8 @@ class ConfigureOpenVPNConnection:
                 self.username = self.username + "+nsm"
             else:
                 self.username = self.username + "+sm"
+
+        logger.info("Appended suffixes: Final username is %s", self.username)
 
     def add_vpn_credentials(self):
         """Add OpenVPN credentials to Proton VPN connection.
